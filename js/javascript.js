d10aa969e8ece8dbff849b1ef5df1afc7e6c7ada
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2016-09-18 15:14:15 +0800

    Changed the CSS to use a class for easier jQuery manipulation. Adding the color remove effect after mouse hovers over a box

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,16 @@
 
 $(document).ready(function(){
-  for (var i = 0; i < 20 ; i++)
-  $('body').append('<div id="box"></div>');
+  for (var i = 0; i < 20 ; i++){
+    $('body').append('<div class="box"></div>')
+  }
+
+  $(document).on("mouseover", ".box", function(){
+    $(this).addClass('.highlighted');
+    $(this).removeClass("box");
+
+  });
+
+
+
+
 });
