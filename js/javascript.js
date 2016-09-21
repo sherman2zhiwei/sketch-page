0c79b90f5a2f29d737b0d54b78f049194762479a
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2016-09-21 19:29:40 +0800

    Added function to change grid size

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,21 @@ $(document).ready(function(){
 
 
 });
+
+var gridSize = function(){
+  $('div').remove();
+  var numberOfBoxesPerSide = prompt("How many boxes do you want per side?");
+  var bodyWidth = $('body').width();
+  var boxWidth = Math.floor(bodyWidth/numberOfBoxesPerSide);
+
+
+  // $('.box').css({'height':boxWidth , 'width':boxWidth});
+  // $('.box').css('height', 1);
+
+  for (var i=0; i < Math.pow(numberOfBoxesPerSide, 2); i++){
+    $('body').append('<div class="box"></div>')
+  }
+
+  $('.box').css({'height':boxWidth , 'width':boxWidth});
+
+}
