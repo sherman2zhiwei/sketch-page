f65c8640057062c866d93f2c2769d0cf26485a73
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2016-09-21 21:15:17 +0800

    Added comments at important sections of the code

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,11 @@
 
 $(document).ready(function(){
   for (var i = 0; i < 256 ; i++){
-    $('body').append('<div class="box"></div>')
+    $('body').append('<div class="box"></div>')    // Add in the boxes one by one using jQuery
   }
 
   $(document).on("mouseover", ".box", function(){
-    $(this).css('background-color', 'white');
-
+    $(this).css('background-color', 'white');    // Turn the boxes to white when a mouse hovers over them
   });
 
 
@@ -15,19 +14,17 @@ $(document).ready(function(){
 });
 
 var gridSize = function(){
-  $('div').remove();
+  $('div').remove();  // Remove the original grid
   var numberOfBoxesPerSide = prompt("How many boxes do you want per side?");
-  var bodyWidth = $('body').width();
-  var boxWidth = Math.floor(bodyWidth/numberOfBoxesPerSide);
+  var bodyWidthBorder = ($('body').width())-((numberOfBoxesPerSide)*3);  // Since a 3px right margin is present after every box, I minus it off to account for the space it takes in the body
+  var boxWidth = Math.floor(bodyWidthBorder/numberOfBoxesPerSide);
 
 
-  // $('.box').css({'height':boxWidth , 'width':boxWidth});
-  // $('.box').css('height', 1);
 
   for (var i=0; i < Math.pow(numberOfBoxesPerSide, 2); i++){
     $('body').append('<div class="box"></div>')
   }
 
-  $('.box').css({'height':boxWidth , 'width':boxWidth});
+  $('.box').css({'height':boxWidth , 'width':boxWidth}); //Readjust the height and width of the boxes as necessary
 
 }
